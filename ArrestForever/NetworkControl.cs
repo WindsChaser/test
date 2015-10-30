a8cd516534f25cb175344ce27e01ccaab8a304e9
Winds <599149192@qq.com>
2015-10-30 21:52:04 +0800

    test02

--- ArrestForever/NetworkControl.cs
+++ ArrestForever/NetworkControl.cs
@@ -5,6 +5,7 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Data;
 using System.Windows.Documents;
 
 namespace ArrestForever
